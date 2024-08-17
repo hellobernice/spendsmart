41cd34dbfffc7d0832461c435d8e14d9ba055508
hellobernice <berniceyan.cao@gmail.com>
2024-08-17 17:56:55 -0400

    Add files via upload

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+import tkinter.messagebox
 
 def main():
     #---------------------------MAIN WINDOW-------------------------------
@@ -30,7 +31,7 @@ def main():
     def calc():
         calcTopLvl = tkinter.Toplevel()
         calcTopLvl.config(bg = "#b5e1c1")
-        calcTopLvl.geometry("1000x600")
+        calcTopLvl.geometry("1200x600")
         calcTopLvl.title("Calculator")
 
         headingFrame = tkinter.Frame(calcTopLvl)
@@ -42,11 +43,92 @@ def main():
         headingCanvas.pack(side = "top")
         instructions = PhotoImage(file = "instructions.png")
         headingCanvas.create_image(50,10, anchor = NW, image = instructions)
+        incomeHeading = PhotoImage(file = "heading.png")
+        headingCanvas.create_image(100,220, anchor = SW, image = incomeHeading)
 
-        heading = PhotoImage(file = "heading.png")
-        headingCanvas.create_image(100,220, anchor = SW, image = heading)
+        frame1 = tkinter.Frame(calcTopLvl)
+        frame1.config(bg = "#b5e1c1")
+        frame1.pack(side = "left")
+        frame2 = tkinter.Frame(calcTopLvl)
+        frame2.config(bg = "#b5e1c1")
+        frame2.pack(side = "left")
+        frame3 = tkinter.Frame(calcTopLvl)
+        frame3.config(bg = "#b5e1c1")
+        frame3.pack(side = "left")
+        frame4 = tkinter.Frame(calcTopLvl)
+        frame4.config(bg = "#b5e1c1")
+        frame4.pack(side = "left")
+        frame5 = tkinter.Frame(calcTopLvl)
+        frame5.config(bg = "#b5e1c1")
+        frame5.pack(side = "left")
         
+        incomeLabel = tkinter.Label(frame1, font = 20,
+                                    text = "Monthly Salary:", bg = "#b5e1c1",
+                                    fg = "#233027")
+        incomeLabel.pack(side = "top", pady = 10, padx = 35)
+        otherLabel = tkinter.Label(frame1, font = 20, text = "Others:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        otherLabel.pack(side = "top", pady = 10, padx = 35)
 
+        incomeEntry = tkinter.Entry(frame2, font = 20)
+        incomeEntry.pack(side = "top", pady = 10, padx = 35)
+        otherEntry = tkinter.Entry(frame2, font = 20)
+        otherEntry.pack(side = "top", pady = 10, padx = 35)
+
+        rentLabel = tkinter.Label(frame3, font = 20, text = "Rent/Mortgage:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        rentLabel.pack(side = "top", pady = 10, padx = 35)
+        utilityLabel = tkinter.Label(frame3, font = 20, text = "Utilities:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        utilityLabel.pack(side = "top", pady = 10, padx = 35)
+        transportLabel = tkinter.Label(frame3, font = 20,
+                                       text = "Transportation:", bg = "#b5e1c1",
+                                       fg = "#233027")
+        transportLabel.pack(side = "top", pady = 10, padx = 35)
+        foodLabel = tkinter.Label(frame3, font = 20, text = "Food:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        foodLabel.pack(side = "top", pady = 10, padx = 35)
+        entLabel = tkinter.Label(frame3, font = 20, text = "Entertainment:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        entLabel.pack(side = "top", pady = 10, padx = 35)
+        miscLabel = tkinter.Label(frame3, font = 20, text = "Miscellaneous:",
+                                   bg = "#b5e1c1", fg = "#233027")
+        miscLabel.pack(side = "top", pady = 10, padx = 35)
+
+        rentEntry = tkinter.Entry(frame4, font = 20)
+        rentEntry.pack(side = "top", pady = 10, padx = 35)
+        utilityEntry = tkinter.Entry(frame4, font = 20)
+        utilityEntry.pack(side = "top", pady = 10, padx = 35)
+        transportEntry = tkinter.Entry(frame4, font = 20)
+        transportEntry.pack(side = "top", pady = 10, padx = 35)
+        foodEntry = tkinter.Entry(frame4, font = 20)
+        foodEntry.pack(side = "top", pady = 10, padx = 35)
+        entEntry = tkinter.Entry(frame4, font = 20)
+        entEntry.pack(side = "top", pady = 10, padx = 35)
+        miscEntry = tkinter.Entry(frame4, font = 20)
+        miscEntry.pack(side = "top", pady = 10, padx = 35)
+
+        def total_earnings():
+            income1 = float(incomeEntry.get())
+            income2 = float(otherEntry.get())
+            total_income = income1 + income2
+
+            expense1 = float(rentEntry.get())
+            expense2 = float(utilityEntry.get())
+            expense3 = float(transportEntry.get())
+            expense4 = float(foodEntry.get())
+            expense5 = float(entEntry.get())
+            expense6 = float(miscEntry.get())
+            total_expenses = expense1 + expense2 + expense3 + expense4 + expense5 + expense6
+            total = total_income - total_expenses
+            tkinter.messagebox.showinfo("Total Earnings", "You have $" +
+                                        str(format(total, '.2f')))
+
+        calcButton = tkinter.Button(frame5, text = "CALCULATE", font = 20,
+                                    bg = "White", fg = "#233027",
+                                    command = total_earnings)
+        calcButton.pack(side = "top")
+        
         tkinter.mainloop()
 
     #--------------------------MAIN WINDOW BUTTONS-------------------------
